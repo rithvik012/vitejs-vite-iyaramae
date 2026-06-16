2e3563a480588a101c683e1eda77aa67929c5a5d
null <null>
2026-06-16 22:55:38 +0530

    ns1

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // This entirely bypasses the lightningcss Vercel bug
+    cssMinify: 'esbuild',
+    // Ensures large files don't trip up the compiler
+    chunkSizeWarningLimit: 2000, 
+  }
 });
